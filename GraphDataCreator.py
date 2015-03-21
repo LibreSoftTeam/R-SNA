a588874c9d4a79230ba301499e3374b7a6d8120d
mghfdez <mafesan.nsn@gmail.com>
2015-03-21 19:15:29 +0100

    funcion 'check_date'

--- GraphDataCreator.py
+++ GraphDataCreator.py
@@ -12,47 +12,62 @@ from time import strftime, gmtime
 print " - Graph Data Creator Started - "
 print " - MAKE SURE CTAGS, PERL AND GIT ARE INSTALLED IN YOUR COMPUTER\r\n"
 
+def help():
+    """
+        Prints usage & description
+    """
+    line = "\r\nNAME\r\n\r\nGraphDataCreator.sh\r\n\r\n"
+    line += "USAGE\r\n\r\n./GrahDataCreator [SHORT-OPTION]\r\n\r\n"
+    line += "EXAMPLE\r\n\r\n"
+    line += "./GraphDataCreator.sh -f 2010-1-1 -t 2011-1-1 -r "
+    line += "git://git.openstack.org/openstack/swift -v\r\n\r\n"
+    
+    line += "DESCRIPTION\r\n\r\n"
+    line += "GraphDataCreator.sh reads information of a Git repository and"
+    line += " outputs two\n.csv files ready to be read to represent"
+    line += " a software community. The files\ncontain pairs "
+    line += "developer-developer meaning that both developers have\nworked "
+    line += "together. One file uses file-scope to create a relationship "
+    line += "while\nthe other narrows relationship down using"
+    line += " a method-scope.\r\n"
+
+    line += "\r\nOPTIONS\r\n\r\n-h\tPrints help page.\r\n\r\n"
+    line += "-f\tStarting date of study. When empty, study start "
+    line += "the beginning of times.\n\tFormat: 2012-12-31\r\n\r\n"
+    line += "-t\tEnding date of study. When empty, current date will "
+    line += "be chosen.\n\tFormat: 2012-12-31\r\n\r\n"
+    line += "-r\tRepository url. When empty, we assume we are "
+    line += "in a directory tracked by Git. \n\tExample: "
+    line += "git://git.openstack.org/openstack/swift\r\n"
+    line += "\r\n-v\tVerbose mode."
+
+    line += "\r\n\r\nDEPENDENCIES\r\n\r\nPerl, Git and Ctags need to run "
+    line += "this script.\r\n\r\nOUTPUT\r\n\r\n"
+    line += "DataMethods.csv-File using relationship-in-method approach\r\n"
+    line += "DataFiles.csv-File using relantionship-in-file approach\r\n"
+    return line
+
+def check_date(date):
+    """
+    Checks if a date has format 'YYYY-MM-DD'
+    """
+    date = str(date)
+    date_fields = date.split('-')
+    ans = 0
+    if len(date_fields) == 3:
+        if date_fields[0] >= 1971:
+            if (int(date_fields[1]) > 0) and (int(date_fields[1]) < 13):
+                if (int(date_fields[2]) > 0) and (int(date_fields[2]) < 32):
+                    ans = 1
+    return ans
+
 class GraphData:
-
-    def help(self):
-        """
-            Prints usage & description
-        """
-        line = "\r\nNAME\r\n\r\nGraphDataCreator.sh\r\n\r\n"
-        line += "USAGE\r\n\r\n./GrahDataCreator [SHORT-OPTION]\r\n\r\n"
-        line += "EXAMPLE\r\n\r\n"
-        line += "./GraphDataCreator.sh -f 2010-1-1 -t 2011-1-1 -r "
-        line += "git://git.openstack.org/openstack/swift -v\r\n\r\n"
-        
-        line += "DESCRIPTION\r\n\r\n"
-        line += "GraphDataCreator.sh reads information of a Git repository and"
-        line += " outputs two\n.csv files ready to be read to represent"
-        line += " a software community. The files\ncontain pairs "
-        line += "developer-developer meaning that both developers have\nworked "
-        line += "together. One file uses file-scope to create a relationship "
-        line += "while\nthe other narrows relationship down using"
-        line += " a method-scope.\r\n"
-
-        line += "\r\nOPTIONS\r\n\r\n-h\tPrints help page.\r\n\r\n"
-        line += "-f\tStarting date of study. When empty, study start "
-        line += "the beginning of times.\n\tFormat: 2012-12-31\r\n\r\n"
-        line += "-t\tEnding date of study. When empty, current date will "
-        line += "be chosen.\n\tFormat: 2012-12-31\r\n\r\n"
-        line += "-r\tRepository url. When empty, we assume we are "
-        line += "in a directory tracked by Git. \n\tExample: "
-        line += "git://git.openstack.org/openstack/swift\r\n"
-        line += "\r\n-v\tVerbose mode."
-
-        line += "\r\n\r\nDEPENDENCIES\r\n\r\nPerl, Git and Ctags need to run "
-        line += "this script.\r\n\r\nOUTPUT\r\n\r\n"
-        line += "DataMethods.csv-File using relationship-in-method approach\r\n"
-        line += "DataFiles.csv-File using relantionship-in-file approach\r\n"
-        return line
 
     def log(self, verbose):
         if verbose:
             print "Verbose mode"
             #os.system('echo "$@"')
+
 
 if __name__ == "__main__":
     my_graph = GraphData()
@@ -84,8 +99,22 @@ if __name__ == "__main__":
                 verbose = True
                 print "Verbose mode on"
             elif value == 'h':
-                print my_graph.help()
+                print help()
     
+    # Checking from and until dates
+    if check_date(from_date):
+        print "Valid starting date: " + from_date
+    else:
+        print "Starting date is wrong. "
+        print "Please use option -h for further information"
+        raise SystemExit
+
+    if check_date(until_date):
+        print "Valid ending date: " + until_date
+    else:
+        print "Ending date is wrong. "
+        print "Please use option -h for further information"
+        raise SystemExit
     
         
 
