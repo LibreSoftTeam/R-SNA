6671cedb03cb4ec44f54242d7055f07faf85ccfc
mghfdez <mafesan.nsn@gmail.com>
2015-03-21 18:37:36 +0100

    Initial options and parameters processing

--- GraphDataCreator.py
+++ GraphDataCreator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-GraphDataCreator Shell-to-Pyhton translation from Christian Coré Ramiro code
+GraphDataCreator Shell(bash)-to-Pyhton translation from Christian Coré Ramiro code
 Miguel Angel Fernandez Sanchez
 """
 
 import os
 import sys
+from time import strftime, gmtime
 
 print " - Graph Data Creator Started - "
 print " - MAKE SURE CTAGS, PERL AND GIT ARE INSTALLED IN YOUR COMPUTER\r\n"
@@ -35,7 +36,7 @@ class GraphData:
         line += "\r\nOPTIONS\r\n\r\n-h\tPrints help page.\r\n\r\n"
         line += "-f\tStarting date of study. When empty, study start "
         line += "the beginning of times.\n\tFormat: 2012-12-31\r\n\r\n"
-        line += "-f\tEnding date of study. When empty, current date will "
+        line += "-t\tEnding date of study. When empty, current date will "
         line += "be chosen.\n\tFormat: 2012-12-31\r\n\r\n"
         line += "-r\tRepository url. When empty, we assume we are "
         line += "in a directory tracked by Git. \n\tExample: "
@@ -48,7 +49,81 @@ class GraphData:
         line += "DataFiles.csv-File using relantionship-in-file approach\r\n"
         return line
 
+    def log(self, verbose):
+        if verbose:
+            print "Verbose mode"
+            #os.system('echo "$@"')
+
 if __name__ == "__main__":
     my_graph = GraphData()
-    print my_graph.help()
+
+    # Initialising options
+    verbose = False
+    from_date = '1971-1-1'
+    until_date = strftime("%Y-%m-%d", gmtime())
+    unamestr = os.uname()[0]
+    if unamestr != 'Linux':
+        print "We are not under Linux, no options available"
+        raise SystemExit
+    url_repo = ""
+
+    # Check introduced paramenters
+    user_param = " ".join(sys.argv)
+    list_param = user_param.split(" -")
+    for value in list_param:
+        value_tmp = value.split()
+        if len(value_tmp) == 2:
+            if value_tmp[0] == 'f':
+                from_date = value_tmp[1]
+            elif value_tmp[0] == 't':
+                until_date = value_tmp[1]
+            elif value_tmp[0] == 'r':
+                url_repo = value_tmp[1]
+        else:
+            if value == 'v':
+                verbose = True
+                print "Verbose mode on"
+            elif value == 'h':
+                print my_graph.help()
+    
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
