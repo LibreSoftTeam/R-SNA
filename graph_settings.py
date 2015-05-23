29cdbd03f2e3373303c87c0eef651122805fa6b2
Miguel Angel Fernandez <mafesan.nsn@gmail.com>
2015-05-23 14:59:34 +0200

    Initial commit

---
+++ graph_settings.py
@@ -0,0 +1,7 @@
+config = {}
+config['v'] = True  # Verbose
+config['f'] = '2005-06-01'  # Starting date of study
+config['t'] = '2011-11-18'  # Ending date of study
+config['r'] = "https://github.com/GNOME/gedit"  # Repository URL
+config['h'] = False  # Shows help
+config['p'] = 8  # Months by period
